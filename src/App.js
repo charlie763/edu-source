55c4e29221ccd433cdc1df68fd1704f1b010ade8
charlie763 <cwisoff@gmail.com>
2020-10-05 14:31:42 -0600

    add Resource Container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ResourceContainer from './containers/ResourceContainer'
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <ResourceContainer />
   );
 }
 
---
+++ src/containers/ResourceContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class ResourceContainer extends React.Component {
+  render(){
+    return(
+      <div>
+        <p>New Resource</p>
+        <p>All Resources/Search</p>
+      </div>
+    )
+  }
+}
+
+export default ResourceContainer
