2cbb45073b07a5c396fd6000f136f46de353915f
charlie763 <cwisoff@gmail.com>
2020-10-06 14:03:11 -0600

    add routing

--- src/containers/ResourceContainer.js
+++ src/containers/ResourceContainer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { addResource } from '../actions/resourceActions'
 import ResourceForm from '../components/ResourceForm'
 
@@ -7,7 +8,9 @@ class ResourceContainer extends React.Component {
   render(){
     return(
       <div>
-        <ResourceForm addResource={this.props.addResource} />
+        <Router> 
+          <Route exact path="/resources/new" render={() => <ResourceForm addResource={this.props.addResource} />} />
+        </Router>
         <p>All Resources/Search</p>
       </div>
     )
