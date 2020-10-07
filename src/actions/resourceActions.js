905b749f856ab6e5f257c01ad54bf637065db24d
charlie763 <cwisoff@gmail.com>
2020-10-07 11:58:58 -0600

    send POST request to api backend when adding resource

--- src/actions/resourceActions.js
+++ src/actions/resourceActions.js
@@ -1,9 +1,21 @@
+const BASE_URL = 'http://localhost:3000/resources'
+
 function addResource(resource){
+  const configObj = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    body: JSON.stringify(resource)
+  }
+
   return (dispatch) => {
     dispatch({
       type: "ADD_RESOURCE",
       resource
     })
+    fetch(BASE_URL, configObj)
   }
 }
 
