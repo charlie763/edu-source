424c9f88f95d4f34e714fdd09bca9711ea008a97
charlie763 <cwisoff@gmail.com>
2020-10-08 15:21:32 -0600

    refactor signup wrapper to handle userFieldForm state and clear inputs on Submit

--- src/components/UserFormFields.js
+++ src/components/UserFormFields.js
@@ -1,27 +1,14 @@
 import React from 'react'
 
-class UserFormFields extends React.Component {
-  state = {
-    username: "",
-    password: ""
-  }
-
-  handleInputChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    })
-  }
-
-  render(){
-    return(
-      <>
-        <label for="username">Username: </label>
-        <input type="text" name="username" value={this.state.username} onChange={this.handleInputChange}/><br/>
-        <label for="password">Password: </label>
-        <input type="password" name="password" value={this.state.password} onChange={this.handleInputChange}/><br/>
-      </>
-    )
-  }
+const UserFormFields = ({ inheritedState, handleInputChange }) => {
+  return(
+    <>
+      <label for="username">Username: </label>
+      <input type="text" name="username" value={inheritedState.username} onChange={handleInputChange}/><br/>
+      <label for="password">Password: </label>
+      <input type="password" name="password" value={inheritedState.password} onChange={handleInputChange}/><br/>
+    </>
+  )
 }
 
 export default UserFormFields
--- src/components/UserSignup.js
+++ src/components/UserSignup.js
@@ -2,14 +2,35 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import UserFormFields from './UserFormFields'
 
-const UserSignup = props => {
-  return(
-    <form>
-      <UserFormFields />
-      <input type="submit" value="Signup"/><br/>
-      <span>Already a user? <Link to="/login">Login</Link></span>
-    </form>
-  )
+class UserSignup extends React.Component {
+  state = {
+    username: "",
+    password: ""
+  }
+
+  handleInputChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+  
+  handleSubmit = e => {
+    e.preventDefault()
+    this.setState({
+      username: "",
+      password: ""
+    })
+  }
+
+  render(){
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <UserFormFields inheritedState={this.state} handleInputChange={this.handleInputChange}/>
+        <input type="submit" value="Signup"/><br/>
+        <span>Already a user? <Link to="/login">Login</Link></span>
+      </form>
+    )
+  }
 }
 
 export default UserSignup
