e833097e87b70bbc36772dde36ccd439166f862d
charlie763 <cwisoff@gmail.com>
2020-10-08 15:02:32 -0600

    give userFormFields component state for controlling input

--- src/components/UserFormFields.js
+++ src/components/UserFormFields.js
@@ -1,13 +1,24 @@
 import React from 'react'
 
 class UserFormFields extends React.Component {
+  state = {
+    username: "",
+    password: ""
+  }
+
+  handleInputChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
   render(){
     return(
       <>
         <label for="username">Username: </label>
-        <input type="text" name="username"/><br/>
+        <input type="text" name="username" value={this.state.username} onChange={this.handleInputChange}/><br/>
         <label for="password">Password: </label>
-        <input type="password" name="password"/><br/>
+        <input type="password" name="password" value={this.state.password} onChange={this.handleInputChange}/><br/>
       </>
     )
   }
