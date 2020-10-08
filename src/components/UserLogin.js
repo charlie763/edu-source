66aa3b47fd48b85d3ebe85722c43704649aef0b3
charlie763 <cwisoff@gmail.com>
2020-10-08 15:26:41 -0600

    refactor UserLogin component logic to handle state and add submit logic

--- src/components/UserLogin.js
+++ src/components/UserLogin.js
@@ -2,14 +2,35 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import UserFormFields from './UserFormFields'
 
-const UserLogin = props => {
-  return(
-    <form>
-      <UserFormFields />
-      <input type="submit" value="Login"/><br/>
-      <span>Not a user? <Link to="/signup">Signup</Link></span>
-    </form>
-  )
+class UserLogin extends React.Component {
+  state = {
+    username: "",
+    password: ""
+  }
+
+  handleInputChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+  
+  handleSubmit = e => {
+    e.preventDefault()
+    this.setState({
+      username: "",
+      password: ""
+    })
+  }
+  
+  render(){
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <UserFormFields inheritedState={this.state} handleInputChange={this.handleInputChange}/>
+        <input type="submit" value="Login"/><br/>
+        <span>Not a user? <Link to="/signup">Signup</Link></span>
+      </form>
+    )
+  }
 }
 
 export default UserLogin
