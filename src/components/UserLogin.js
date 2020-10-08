3d3006e2046024e9eff98843abfdab681aa5f796
charlie763 <cwisoff@gmail.com>
2020-10-08 14:52:46 -0600

    link to signup page from login

--- src/components/UserLogin.js
+++ src/components/UserLogin.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import UserFormFields from './UserFormFields'
 
 const UserLogin = props => {
   return(
     <form>
       <UserFormFields />
-      <input type="submit" value="Login"/>
+      <input type="submit" value="Login"/><br/>
+      <span>Not a user? <Link to="/signup">Signup</Link></span>
     </form>
   )
 }
