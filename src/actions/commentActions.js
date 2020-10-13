b4f758ed31023d4372740158db69e4e390813b56
charlie763 <cwisoff@gmail.com>
2020-10-13 15:41:26 -0600

    add comment action

---
+++ src/actions/commentActions.js
@@ -0,0 +1,17 @@
+const BASE_URL = 'http://localhost:3000/comments'
+
+function addComment(comment){
+  const configObj = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    body: JSON.stringify(comment)
+  }
+
+  return dispatch => {
+    dispatch({type: "ADD_COMMENT", comment})
+    fetch(BASE_URL, configObj)
+  }
+}
