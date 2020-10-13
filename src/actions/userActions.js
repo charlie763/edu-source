2f3a2895a21cc109bd16fa69d81d118acd8a593e
charlie763 <cwisoff@gmail.com>
2020-10-13 10:13:37 -0600

    build loginUser action

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,5 +1,5 @@
 import * as Cookies from "js-cookie"
-const BASE_URL = 'http://localhost:3000/users'
+const BASE_URL = 'http://localhost:3000'
 
 function createUser(user){
   const configObj = {
@@ -12,7 +12,7 @@ function createUser(user){
   }
 
   return dispatch => {
-    fetch(BASE_URL, configObj)
+    fetch(BASE_URL.concat('/users'), configObj)
       .then(resp=>resp.json())
       .then(userData => {
         if (userData.valid === "true"){
@@ -26,4 +26,29 @@ function createUser(user){
   }
 }
 
-export { createUser }
+function loginUser(user){
+  const configObj = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    body: JSON.stringify(user)
+  }
+
+  return dispatch => {
+    fetch(BASE_URL.concat('/login'), configObj)
+      .then(resp=>resp.json())
+      .then(userData => {
+        if (userData.valid === "true"){
+          Cookies.remove("session")
+          Cookies.set("session", userData.token, { expires: 14 })
+          return dispatch({type: "LOGIN_USER", user: userData.user, valid: "true"})
+        } else {
+          return dispatch({type: "INVALID_USER", errors: userData.errorMessages})
+        }
+      })
+  }
+}
+
+export { createUser, loginUser }
