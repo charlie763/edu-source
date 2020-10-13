e79fb2649c1b33fb922890d07d21b68f0b223dad
charlie763 <cwisoff@gmail.com>
2020-10-13 11:55:01 -0600

    make user actions more DRY

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,53 +1,44 @@
 import * as Cookies from "js-cookie"
 const BASE_URL = 'http://localhost:3000'
 
-function createUser(user){
-  const configObj = {
+function buildPostObj(user){
+  return ({
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
       Accept: 'application/json'
     },
     body: JSON.stringify(user)
+  })
+}
+
+function validateUser(dispatch, userData){
+  if (userData.valid === "true"){
+    Cookies.remove("session")
+    Cookies.set("session", userData.token, { expires: 14 })
+    return dispatch({type: "LOGIN_USER", user: userData.user, valid: "true"})
+  } else {
+    return dispatch({type: "INVALID_USER", errors: userData.errorMessages})
   }
+}
+
+function createUser(user){
+  const configObj = buildPostObj(user)
 
   return dispatch => {
     fetch(BASE_URL.concat('/users'), configObj)
       .then(resp=>resp.json())
-      .then(userData => {
-        if (userData.valid === "true"){
-          Cookies.remove("session")
-          Cookies.set("session", userData.token, { expires: 14 })
-          return dispatch({type: "LOGIN_USER", user: userData.user, valid: "true"})
-        } else {
-          return dispatch({type: "INVALID_USER", errors: userData.errorMessages})
-        }
-      })
+      .then(userData => validateUser(dispatch, userData))
   }
 }
 
 function loginUser(user){
-  const configObj = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json'
-    },
-    body: JSON.stringify(user)
-  }
+  const configObj = buildPostObj(user)
 
   return dispatch => {
     fetch(BASE_URL.concat('/login'), configObj)
       .then(resp=>resp.json())
-      .then(userData => {
-        if (userData.valid === "true"){
-          Cookies.remove("session")
-          Cookies.set("session", userData.token, { expires: 14 })
-          return dispatch({type: "LOGIN_USER", user: userData.user, valid: "true"})
-        } else {
-          return dispatch({type: "INVALID_USER", errors: userData.errorMessages})
-        }
-      })
+      .then(userData => validateUser(dispatch, userData))
   }
 }
 
