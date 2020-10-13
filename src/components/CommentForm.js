3a280bc2ed6c6ef6b2b6758a7261127a57b2406f
charlie763 <cwisoff@gmail.com>
2020-10-13 15:37:09 -0600

    stub out logic for submitting comment form

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -6,16 +6,14 @@ class CommentForm extends React.Component{
     text: ""
   }
 
-  // onSubmit={e => handleSubmit.call(this, {
-  //   e,
-  //   callback: this.props.addComment({...state, userId: this.props.user.id}),
-  //   currentState: this.state,
-  //   cleanState: {text: ""}
-  // })}
-
   render(){
     return(
-      <form>
+      <form onSubmit={e => handleSubmit.call(this, {
+        e,
+        callback: this.props.addComment,
+        currentState: {...this.state, userId: this.props.user.id},
+        cleanState: {text: ""}
+      })}>
         <textarea name="text" value={this.state.text} placeholder="write comment here..." onChange={e => handleInputChange.call(this, e)} /><br/>
         <input type="submit" value="Post Comment"/>
       </form>
