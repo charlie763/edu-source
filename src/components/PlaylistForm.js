80f0fee8eea47798862547b35dda593f72823373
charlie763 <cwisoff@gmail.com>
2020-10-14 11:52:44 -0600

    stub out playlist form

--- src/components/PlaylistForm.js
+++ src/components/PlaylistForm.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import { handleInputChange } from '../utilities'
 import PlaylistContext from './PlaylistContext'
 
 class PlaylistForm extends React.Component{
+  state ={
+    name: ""
+  }
   render(){
     return (
       <div>
-        PlaylistForm
+        <form>
+          <label>Add New Playlist: </label>
+          <input type="text" name="name" value={this.state.name} onChange={e => handleInputChange.call(this, e)} /><br/>
+          <label>Select Playlist: </label>
+          <select></select>
+        </form>
         <PlaylistContext 
           context={this.props.location.context}
           displayGrade={this.props.displayGrade}
