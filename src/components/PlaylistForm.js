87f10a85dd9f56116c20b6b43238ccbb3d528262
charlie763 <cwisoff@gmail.com>
2020-10-14 14:51:43 -0600

    handle submit for select playlist

--- src/components/PlaylistForm.js
+++ src/components/PlaylistForm.js
@@ -3,12 +3,19 @@ import { handleInputChange, handleSubmit } from '../utilities'
 import PlaylistContext from './PlaylistContext'
 
 class PlaylistForm extends React.Component{
-  state ={
-    name: ""
+  state = {
+    name: "",
+    selectName: ""
   }
 
   componentDidMount(){
     this.props.fetchPlaylists()
+  }
+
+  handleSelectChange = e => {
+    this.setState({
+      [e.target.name]: e.target.options[e.target.selectedIndex].text 
+    })
   }
 
   render(){
@@ -17,16 +24,24 @@ class PlaylistForm extends React.Component{
         <form onSubmit={e => handleSubmit.call(this, {
           e,
           callback: this.props.addPlaylist,
-          currentState: this.state,
-          cleanState: {name: ""}
+          currentState: {name: this.state.name},
+          cleanState: {name: "", selectName: ""}
         })}>
           <label>Add New Playlist: </label>
           <input type="text" name="name" value={this.state.name} onChange={e => handleInputChange.call(this, e)} /><br/>
           <input type="submit" value="add"/>
         </form>
-        <form>
+        <form onSubmit={e => {
+          const name = this.state.selectName.length > 0 ? this.state.selectName : this.props.playlists[0]
+          handleSubmit.call(this, {
+            e,
+            callback: this.props.addPlaylist,
+            currentState: { name },
+            cleanState: {name: "", selectName: ""}
+          })
+        }}>
           <label>Select Playlist: </label>
-          <select>
+          <select name="selectName" onChange={this.handleSelectChange}>
             {this.props.playlists.map(playlist => (
               <option key={playlist.id}>{playlist.name}</option>
             ))}
