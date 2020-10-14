0e2b1af8e92a8f05e26218de384f6ac838eaeb04
charlie763 <cwisoff@gmail.com>
2020-10-14 09:25:39 -0600

    write LOAD COMMENTS and ADD COMMENTs cases for comments reducer

--- src/reducers/commentsReducer.js
+++ src/reducers/commentsReducer.js
@@ -1,6 +1,6 @@
 import cuid from 'cuid'
 
-function commentsReducer(state = { list: [], loadStatus: null}, action){
+function commentsReducer(state = { list: [], loadStatus: null, resourceLoaded: null}, action){
   switch(action.type){
     case "ADD_COMMENT":
       const newComment = {
@@ -10,6 +10,18 @@ function commentsReducer(state = { list: [], loadStatus: null}, action){
       return {
         list: [...state.list, newComment],
         loadStatus: state.loadStatus
+      }
+    case "LOAD_COMMENTS":
+      return {
+        list: [...state.list],
+        loadStatus: "pending",
+        resourceLoaded: action.resourceId
+      }
+    case "ADD_COMMENTS":
+      return {
+        list: [...action.comments],
+        loadStatus: "complete",
+        resourceLoaded: state.resourceLoaded
       }
     default: 
       return state 
