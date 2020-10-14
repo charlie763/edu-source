81e3c43f76d2bf0c7172a69d6e1a119df13b0300
charlie763 <cwisoff@gmail.com>
2020-10-14 13:38:59 -0600

    automatically pull in token for fetchPlaylists action

--- src/actions/playlistActions.js
+++ src/actions/playlistActions.js
@@ -1,6 +1,8 @@
+import * as Cookies from "js-cookie"
 const BASE_URL = 'http://localhost:3000/playlists'
 
-function fetchPlaylists(token){
+function fetchPlaylists(){
+  let token = Cookies.get("eduResourceSession");
   return dispatch => {
     dispatch({type: 'LOAD_PLAYLISTS'})
     fetch(BASE_URL + `/?token=${token}`)
