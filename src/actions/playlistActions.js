eafd1a6cf225d725c7c114afdfc5d90cddf75bcb
charlie763 <cwisoff@gmail.com>
2020-10-14 13:37:25 -0600

    add fetchPlaylists action

--- src/actions/playlistActions.js
+++ src/actions/playlistActions.js
@@ -1,3 +1,15 @@
-function fetchPlaylists(){
-  
+const BASE_URL = 'http://localhost:3000/playlists'
+
+function fetchPlaylists(token){
+  return dispatch => {
+    dispatch({type: 'LOAD_PLAYLISTS'})
+    fetch(BASE_URL + `/?token=${token}`)
+      .then(resp => resp.json())
+      .then(playlists => dispatch({
+        type: "ADD_PLAYLISTS",
+        playlists
+      }))
+  }
 }
+
+export { fetchPlaylists }
