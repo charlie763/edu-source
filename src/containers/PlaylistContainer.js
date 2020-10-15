54d40ed91eeaf47a48eda6eaf990c38878257ab1
charlie763 <cwisoff@gmail.com>
2020-10-15 07:31:38 -0600

    stub out routing for playlist container

--- src/containers/PlaylistContainer.js
+++ src/containers/PlaylistContainer.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { Route, Switch} from 'react-router-dom'
+import Playlists from '../components/Playlists'
+import Playlist from '../components/Playlist'
 
 class PlaylistContainer extends React.Component{
   render(){
     return(
       <div>
-        PlaylistContainer
+        <Switch>
+          <Route path={`${this.props.match.path}/:id`}>
+            <Playlist />
+          </Route>
+          <Route path={`${this.props.match.path}`}>
+            <Playlists />
+          </Route>
+        </Switch>
       </div>
     )
   }
