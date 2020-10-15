24c9ba20cbabea62bbf2076bdd0de59f5f450019
charlie763 <cwisoff@gmail.com>
2020-10-15 07:55:16 -0600

    map resource state to props in playlist container

---
+++ src/components/Playlist.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Playlist = props => (
+  <div>
+    <h2>{props.playlist.name}</h2>
+  </div>
+)
+
+export default Playlist
--- src/containers/PlaylistContainer.js
+++ src/containers/PlaylistContainer.js
@@ -8,15 +8,21 @@ import Playlist from '../components/Playlist'
 class PlaylistContainer extends React.Component{
   componentDidMount(){
     this.props.fetchPlaylists()
+    if (!this.props.resourcesLoadStatus){
+      this.props.fetchResources()
+    }
   }
 
   render(){
     return(
       <div>
         <Switch>
-          <Route path={`${this.props.match.path}/:id`}>
-            <Playlist />
-          </Route>
+          <Route path={`${this.props.match.path}/:id`} render={props => { 
+            debugger;
+            <Playlist 
+
+            />          
+          }}/>
           <Route path={`${this.props.match.path}`} render={props => 
             <Playlists {...props} playlists={this.props.playlists}/>          
           }/>
@@ -26,4 +32,10 @@ class PlaylistContainer extends React.Component{
   }
 }
 
-export default connect(state => ({ playlists: state.playlists.list }), { fetchPlaylists })(PlaylistContainer)
+const mapStateToProps = state => ({
+  playlists: state.playlists.list,
+  resourcesLoadStatus: state.resources.loadStatus,
+  resources: state.resources.list
+})
+
+export default connect(mapStateToProps, { fetchPlaylists })(PlaylistContainer)
