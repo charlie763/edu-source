3be98cd103e77ab098c82f7a267fa79279362139
charlie763 <cwisoff@gmail.com>
2020-10-15 07:42:56 -0600

    connect playlist container to store and fetch playlists

--- src/containers/PlaylistContainer.js
+++ src/containers/PlaylistContainer.js
@@ -1,9 +1,15 @@
 import React from 'react'
-import { Route, Switch} from 'react-router-dom'
+import { connect } from 'react-redux'
+import { Route, Switch } from 'react-router-dom'
+import { fetchPlaylists } from '../actions/playlistActions'
 import Playlists from '../components/Playlists'
 import Playlist from '../components/Playlist'
 
 class PlaylistContainer extends React.Component{
+  componentDidMount(){
+    this.props.fetchPlaylists()
+  }
+
   render(){
     return(
       <div>
@@ -20,4 +26,4 @@ class PlaylistContainer extends React.Component{
   }
 }
 
-export default PlaylistContainer
+export default connect(null, { fetchPlaylists })(PlaylistContainer)
