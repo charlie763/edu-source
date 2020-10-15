d4568c289305d4152fb128aab25befc3a87f20cf
charlie763 <cwisoff@gmail.com>
2020-10-15 15:53:32 -0600

    redirect after submission of playlist select form

--- src/components/PlaylistForm.js
+++ src/components/PlaylistForm.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import { Redirect } from 'react-router-dom'
 import PlaylistAddForm from './PlaylistAddForm'
 import PlaylistContext from './PlaylistContext'
 
 class PlaylistForm extends React.Component{
   state = {
     name: "",
-    selectId: null
+    selectId: null,
+    submitted: false
   }
 
   componentDidMount(){
@@ -35,34 +37,41 @@ class PlaylistForm extends React.Component{
     this.props.addResourceToPlaylist(this.props.location.state.resourceId, id)
     this.setState({
       name: "",
-      selectId: null
+      selectId: null,
+      submitted: true
     })
   }
 
   render(){
-    return (
-      <div>
-        <PlaylistAddForm 
-          addPlaylist={this.props.addPlaylist}
-          clearState={{name: "", selectId: null}}
-          context={this.props.location.context}
-          resourceId={this.props.location.state.resourceId}
-        />
-        <form onSubmit={this.handleSelectSubmit}>
-          <label>Select Playlist: </label>
-          <select onChange={this.handleSelectChange}>
-            {this.props.playlists.map(playlist => (
-              <option id={playlist.id} key={playlist.id}>{playlist.name}</option>
-            ))}
-          </select><br/>
-          <input type="submit" value="Select"/>
-        </form>
-        <PlaylistContext 
-          context={this.props.location.context}
-          resourceId={this.props.location.state.resourceId}
-        />
-      </div>
-    )
+    if (this.state.submitted){
+      const context = this.props.location.context
+      const redirectUrl = context === "resource" ? `${context}s/${this.props.location.state.resourceId}` : context
+      return <Redirect to={`/${redirectUrl}`} />
+    } else {
+      return (
+        <div>
+          <PlaylistAddForm 
+            addPlaylist={this.props.addPlaylist}
+            clearState={{name: "", selectId: null}}
+            context={this.props.location.context}
+            resourceId={this.props.location.state.resourceId}
+          />
+          <form onSubmit={this.handleSelectSubmit}>
+            <label>Select Playlist: </label>
+            <select onChange={this.handleSelectChange}>
+              {this.props.playlists.map(playlist => (
+                <option id={playlist.id} key={playlist.id}>{playlist.name}</option>
+              ))}
+            </select><br/>
+            <input type="submit" value="Select"/>
+          </form>
+          <PlaylistContext 
+            context={this.props.location.context}
+            resourceId={this.props.location.state.resourceId}
+          />
+        </div>
+      )
+    }
   }
 }
 
