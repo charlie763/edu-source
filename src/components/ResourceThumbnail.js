37d5f0a7f9dff7ad208a3339a56cb5665d88d09e
charlie763 <cwisoff@gmail.com>
2020-10-16 08:37:34 -0600

    add initial card styling

--- src/components/ResourceThumbnail.js
+++ src/components/ResourceThumbnail.js
@@ -4,25 +4,27 @@ import { displayGrade } from '../utilities'
 
 const ResourceThumbnail = props => {
   return (
-    <div>
-      <h3>{props.resource.title}</h3>
-      {props.playlist ? 
-        <button onClick={() => props.removeResourceFromPlaylist(props.resource.id, props.playlist.id)}>
-          Remove
-        </button>
-        :
-        <Link to={{
-          pathname: `/resources/playlists/new`,
-          context: "resources", 
-          state: {resourceId: props.resource.id}
-        }}>
-          Add To Bookshelf
-        </Link>
-      }
-     <br/>
-      <span>{props.resource.subject} Grades: {displayGrade(props.resource.lowerGradeBound)} to {displayGrade(props.resource.upperGradeBound)}</span>
-      <p>{props.resource.description}</p>
-      <Link to={`/resources/${props.resource.id}`}>See More</Link>
+    <div className="card">
+      <div className="card-body">
+        <h4 classNmae="card-title">{props.resource.title}</h4>
+        {props.playlist ? 
+          <button onClick={() => props.removeResourceFromPlaylist(props.resource.id, props.playlist.id)}>
+            Remove
+          </button>
+          :
+          <Link to={{
+            pathname: `/resources/playlists/new`,
+            context: "resources", 
+            state: {resourceId: props.resource.id}
+          }}>
+            Add To Bookshelf
+          </Link>
+        }
+      <br/>
+        <span>{props.resource.subject} Grades: {displayGrade(props.resource.lowerGradeBound)} to {displayGrade(props.resource.upperGradeBound)}</span>
+        <p>{props.resource.description}</p>
+        <Link to={`/resources/${props.resource.id}`}>See More</Link>
+      </div>
     </div>
   )
 }
--- src/components/Resources.js
+++ src/components/Resources.js
@@ -3,7 +3,7 @@ import ResourceThumbnail from './ResourceThumbnail'
 
 const Resources = props => {
   return (
-    <div>
+    <div className="card-columns">
       {props.resources.map(resource => <ResourceThumbnail 
         key={resource.id} 
         resource={resource}
