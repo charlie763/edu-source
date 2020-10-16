78b2e31ef4c62aa07ca074fde0f6badfd774aa22
charlie763 <cwisoff@gmail.com>
2020-10-16 07:58:41 -0600

    install/import bootstrap and begin navbar styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,29 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-const NavBar = props => {
+const NavBar = () => {
   return(
-    <div>
-      <NavLink to="/">Home</NavLink><br />
-      <NavLink to="/login">Login</NavLink><br />
-      <NavLink to="/signup">Sign Up</NavLink><br />
-      <NavLink to="/resources">Resources</NavLink><br />
-      <NavLink to="/bookshelves">Book Shelves</NavLink><br />
-    </div>
+    <nav id="navbar" class="navbar navbar-expand-lg navbar-light justify-content-between secondary-background">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-menu" aria-controls="nav-menu" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="nav-menu">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li class="nav-item">
+            <NavLink to="/bookshelves">Book Shelves</NavLink>
+          </li>
+          <li class="nav-item">
+            <NavLink to="/resources">Resources</NavLink>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <NavLink to="/login">Login</NavLink>
+      </div>
+    </nav>
   )
 }
 
