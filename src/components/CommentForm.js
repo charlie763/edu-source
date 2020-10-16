881e190b8b03d544c8cd839e249cb880867a848c
charlie763 <cwisoff@gmail.com>
2020-10-16 09:54:45 -0600

    make comment form a modal

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -13,23 +13,25 @@ class CommentForm extends React.Component{
       return <Redirect to={`/resources/${this.props.resourceId}`}/>
     } else {
       return(
-        <div class="modal" tabindex="-1" role="dialog">
-          <div class="modal-dialog" role="document">
-            <div class="modal-content">
-              <form onSubmit={e => handleSubmit.call(this, {
-                e,
-                callback: this.props.addComment,
-                currentState: {
-                  ...this.state, 
-                  userId: this.props.user.userId, 
-                  resourceId: this.props.resourceId,
-                  user: this.props.user
-                },
-                clearState: {text: ""}
-              })}>
-                <textarea name="text" value={this.state.text} placeholder="write comment here..." onChange={e => handleInputChange.call(this, e)} /><br/>
-                <input type="submit" value="Post Comment"/>
-              </form>
+        <div className="modal" id="comment-form" tabindex="-1" role="dialog">
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-body">
+                <form onSubmit={e => handleSubmit.call(this, {
+                  e,
+                  callback: this.props.addComment,
+                  currentState: {
+                    ...this.state, 
+                    userId: this.props.user.userId, 
+                    resourceId: this.props.resourceId,
+                    user: this.props.user
+                  },
+                  clearState: {text: ""}
+                })}>
+                  <textarea name="text" value={this.state.text} placeholder="write comment here..." onChange={e => handleInputChange.call(this, e)} /><br/>
+                  <input type="submit" value="Post Comment"/>
+                </form>
+              </div>
             </div>
           </div>
         </div>
