b3c88ce59543e5c8c6c9b406752e8c83220a59a3
charlie763 <cwisoff@gmail.com>
2020-10-16 08:18:08 -0600

    sytle navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,25 +3,25 @@ import { NavLink } from 'react-router-dom'
 
 const NavBar = () => {
   return(
-    <nav id="navbar" class="navbar navbar-expand-lg navbar-light justify-content-between secondary-background">
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-menu" aria-controls="nav-menu" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+    <nav id="navbar" className="navbar navbar-expand-lg navbar-light justify-content-between secondary-background">
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-menu" aria-controls="nav-menu" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="nav-menu">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <NavLink to="/">Home</NavLink>
+      <div className="collapse navbar-collapse" id="nav-menu">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <NavLink className="secondary-text" to="/">Home</NavLink>
           </li>
-          <li class="nav-item">
-            <NavLink to="/bookshelves">Book Shelves</NavLink>
+          <li className="nav-item">
+            <NavLink className="secondary-text" to="/bookshelves">Book Shelves</NavLink>
           </li>
-          <li class="nav-item">
-            <NavLink to="/resources">Resources</NavLink>
+          <li className="nav-item">
+            <NavLink className="secondary-text" to="/resources">Resources</NavLink>
           </li>
         </ul>
       </div>
       <div>
-        <NavLink to="/login">Login</NavLink>
+        <NavLink className="secondary-text" to="/login">Login</NavLink>
       </div>
     </nav>
   )
