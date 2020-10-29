83701455d6171d951738d7a6c2460e1caca041cd
charlie763 <cwisoff@gmail.com>
2020-10-29 16:57:33 -0600

    stub out test for commentForm

--- src/containers/CommentContainer.js
+++ src/containers/CommentContainer.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 import { Route } from 'react-router-dom'
 import { authorizeUser } from '../actions/userActions'
 import { addComment, fetchComments } from '../actions/commentActions'
-import CommentForm from '../components/comments/CommentForm'
+import CommentForm from '../components/comments/commentForm/CommentForm'
 import Comments from '../components/comments/Comments'
 import UserContainer from './UserContainer'
 
