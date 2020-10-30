0a9c948a05e5f805935bad16b696a3618b285610
charlie763 <cwisoff@gmail.com>
2020-10-30 14:30:30 -0600

    create snapshot and associated test

--- src/components/comments/commentForm/__tests__/commentForm.test.js
+++ src/components/comments/commentForm/__tests__/commentForm.test.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import CommentForm from '../CommentForm'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { addComment } from '../../../../actions/commentActions'
+import renderer from 'react-test-renderer'
 
 it ('renders without crashing', () => {
   const div = document.createElement('div')
@@ -10,4 +11,13 @@ it ('renders without crashing', () => {
     resourceId={1} 
     user={{username: "Charlie", id: 1}}
     addComment={addComment}/></Router>, div)
+})
+
+it ('matches the snapshot', () => {
+  const tree = renderer.create(<Router><CommentForm 
+    resourceId={1} 
+    user={{username: "Charlie", id: 1}}
+    addComment={addComment}/></Router>
+    ).toJSON()
+  expect(tree).toMatchSnapshot()
 })
