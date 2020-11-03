2ea06bc648729c0aeabc1430152b7d56f2885981
charlie763 <cwisoff@gmail.com>
2020-11-03 14:53:46 -0700

    stub out tests to write for comments

---
+++ src/components/comments/comments/__tests__/comments.test.js
@@ -0,0 +1,19 @@
+it ('renders without crashing', ()=> {
+
+})
+
+it ('matches the snapshot given mocked comments data', ()=> {
+
+})
+
+it ('displays the correct number of comments', ()=> {
+
+})
+
+it ('displays the name of the user who made the comment', ()=> {
+
+})
+
+it ('displays the text of the comment', ()=> {
+  
+})
