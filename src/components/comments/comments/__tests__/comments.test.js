c8f38925b6b934e84f8acf1a51212d1f5859972a
charlie763 <cwisoff@gmail.com>
2020-11-03 15:00:20 -0700

    test that comments component renders without crashing

--- src/components/comments/comments/__tests__/comments.test.js
+++ src/components/comments/comments/__tests__/comments.test.js
@@ -1,5 +1,35 @@
-it ('renders without crashing', ()=> {
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Comments from '../Comments'
 
+const comments = [
+  {
+    id: 4,
+    text: 'My kids love this!',
+    resource_id: 4,
+    user_id: 3,
+    created_at: '2020-10-21T16:54:41.856Z',
+    updated_at: '2020-10-21T16:54:41.856Z',
+    user: {
+      username: 'Naninbah'
+    }
+  },
+  {
+    id: 5,
+    text: 'This is a test comment!',
+    resource_id: 4,
+    user_id: 1,
+    created_at: '2020-10-21T16:54:41.856Z',
+    updated_at: '2020-10-21T16:54:41.856Z',
+    user: {
+      username: 'Charlie'
+    }
+  }
+]
+
+it ('renders without crashing', ()=> {
+  const div = document.createElement('div')
+  ReactDOM.render(<Comments comments={comments}/>, div)
 })
 
 it ('matches the snapshot given mocked comments data', ()=> {
@@ -15,5 +45,5 @@ it ('displays the name of the user who made the comment', ()=> {
 })
 
 it ('displays the text of the comment', ()=> {
-  
+
 })
