8f2ab0ddd79e3d34c32955b7298c973c32227355
charlie763 <cwisoff@gmail.com>
2020-11-03 16:45:13 -0700

    start writing test for making sure fetchComments gets called

--- src/containers/commentContainer/__tests__/commentContainer.test.js
+++ src/containers/commentContainer/__tests__/commentContainer.test.js
@@ -6,21 +6,73 @@ import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from '../../../reducers/rootReducer'
+import { fetchComments } from '../../../actions/commentActions'
+import { act } from 'react-dom/test-utils'
+
+const mockFns = {
+  fetchComments
+}
+
+const comments = [
+  {
+    id: 4,
+    text: 'My kids love this!',
+    resource_id: 4,
+    user_id: 3,
+    created_at: '2020-10-21T16:54:41.856Z',
+    updated_at: '2020-10-21T16:54:41.856Z',
+    user: {
+      username: 'Nanibah'
+    }
+  },
+  {
+    id: 5,
+    text: 'This is a test comment!',
+    resource_id: 4,
+    user_id: 1,
+    created_at: '2020-10-21T16:54:41.856Z',
+    updated_at: '2020-10-21T16:54:41.856Z',
+    user: {
+      username: 'Charlie'
+    }
+  }
+]
+
+const div = document.createElement('div')
+const store = createStore(rootReducer, applyMiddleware(thunk))
 
 it ('renders without crashing', ()=> {
-  const div = document.createElement('div')
-  const store = createStore(rootReducer, applyMiddleware(thunk))
   ReactDOM.render(<Provider store={store}>
                     <Router>
                       <CommentContainer 
                         relativePath={"/resources/:id"} 
                         resourceId={1} />
                     </Router>
-                  </Provider>, div)
+                  </Provider>, 
+                  div
+                  )
 })
 
 it ('fetches comments upon rendering', ()=> {
-
+  //fix: need to look at testing redux docs and add mockStore and then connect the mock function because currently the real fn is being connected
+  const spy = jest.spyOn(mockFns, "fetchComments").mockImplementation(() => 
+      Promise.resolve({
+        json: () => Promise.resolve(comments)
+      })
+    )
+  act(()=> {
+    ReactDOM.render(<Provider store={store}>
+      <Router>
+        <CommentContainer 
+          relativePath={"/resources/:id"} 
+          resourceId={1} />
+      </Router>
+    </Provider>, 
+    div
+    )
+  })
+  expect(spy).toHaveBeenCalled()
+  mockFns.fetchComments.mockRestore()
 })
 
 it ('authorizes users upon rendering', ()=> {
