195afab29e1cf55980cd2f6709e9f2970280229d
charlie763 <cwisoff@gmail.com>
2020-11-03 15:59:31 -0700

    stub out tests for commentContainer

---
+++ src/containers/commentContainer/__tests__/commentContainer.test.js
@@ -0,0 +1,29 @@
+it ('renders without crashing', ()=> {
+
+})
+
+it ('fetches comments upon rendering', ()=> {
+
+})
+
+it ('authorizes users upon rendering', ()=> {
+
+})
+
+it ('fetches comments again if the resources loads after the first fetch', ()=> {
+
+})
+
+it ('renders a Comments component', ()=> {
+  
+})
+
+describe('new comment routing', ()=> {
+  it ('renders a CommentForm component if there is a valid user', ()=> {
+
+  })
+  it ('renders a login modal if there is NOT a valid user', ()=> {
+
+  })
+})
+
