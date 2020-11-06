59d9871b7499beea5c247b1b51a517cf57f8e0f8
charlie763 <cwisoff@gmail.com>
2020-11-06 09:18:57 -0700

    test that playlist renders something regardless of the playlist load status

--- src/components/playlists/playlist/Playlist.js
+++ src/components/playlists/playlist/Playlist.js
@@ -4,15 +4,15 @@ import ResourceThumbnail from '../../resources/ResourceThumbnail'
 const Playlist = props => {
   if (!props.loadStatus || props.loadStatus === "pending"){
     return(
-      <div>Loading...</div>
+      <div id="playlist-wrapper" data-testid="playlist-wrapper">Loading...</div>
     )
   } else if (!props.playlist){
     return(
-      <div>Resource Not Found</div>
+      <div data-testid="playlist-wrapper">Resource Not Found</div>
     )
   } else {
     return(
-      <div>
+      <div data-testid="playlist-wrapper">
         <h2 className="my-3">{props.playlist.name}</h2>
         {props.playlist.resources.map(resource => 
           <div key={resource.id} className="my-2">
--- src/components/playlists/playlist/__tests__/playlist.test.js
+++ src/components/playlists/playlist/__tests__/playlist.test.js
@@ -1,6 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Playlist from '../Playlist'
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow, configure } from 'enzyme'
+
+configure({ adapter: new Adapter() });
+
+const mockProps = {
+  playlist: {
+    id: 1,
+    name: "Default Bookshelf",
+    resources: [
+
+    ],
+    user_id: 1
+  },
+  loadStatus: null
+  // removeResourceFromPlaylist: this.props.removeResourceFromPlaylist,
+  // isResourceInPlaylist: resourceId => isResourceInPlaylist.call(this, resourceId)
+}
 
 it ('renders without crashing', () => {
   const div = document.createElement('div')
@@ -8,7 +26,14 @@ it ('renders without crashing', () => {
 })
 
 it ('renders something regardless of whether or not a playlist gets loaded from the api', () => {
-
+  const wrapper = shallow(<Playlist {...mockProps} />)
+  expect(wrapper.exists('[data-testid="playlist-wrapper"]')).toBe(true)
+  wrapper.setProps({ loadStatus: "pending" })
+  expect(wrapper.exists('[data-testid="playlist-wrapper"]')).toBe(true)
+  wrapper.setProps({ loadStatus: "complete" })
+  expect(wrapper.exists('[data-testid="playlist-wrapper"]')).toBe(true)
+  wrapper.setProps({ playlist: null })
+  expect(wrapper.exists('[data-testid="playlist-wrapper"]')).toBe(true)
 })
 
 it ('matches the snapshot given mocked playlist data', () => {
