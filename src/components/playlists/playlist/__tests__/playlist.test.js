d579b39c8d53611266dc41a3a8aa368ef5f783bb
charlie763 <cwisoff@gmail.com>
2020-11-06 08:44:49 -0700

    test that Playist component renders without crashing

--- src/components/playlists/playlist/__tests__/playlist.test.js
+++ src/components/playlists/playlist/__tests__/playlist.test.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
+import Playlist from '../Playlist'
 
 it ('renders without crashing', () => {
-  
+  const div = document.createElement('div')
+  ReactDOM.render(<Playlist />, div)
 })
 
 it ('renders something regardless of whether or not a playlist gets loaded from the api', () => {
