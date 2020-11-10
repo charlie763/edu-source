04bcc404f44d53ed98b3da28fec93b48f12b67b7
charlie763 <cwisoff@gmail.com>
2020-11-09 17:25:10 -0700

    stub out tests to write

--- src/components/playlists/playlistAddForm/__tests__/playlistAddForm.test.js
+++ src/components/playlists/playlistAddForm/__tests__/playlistAddForm.test.js
@@ -1,2 +1,25 @@
 import React from 'react'
 
+it ('renders without crashing', () => {
+
+})
+
+it ('matches the snapshot', () => {
+
+})
+
+it ('redirects to the /bookshelves route after submission if brought up from that route', ()=> {
+
+})
+
+it ('redirects to the /bookshelves route after submission if brought up from that route', ()=> {
+
+})
+
+it ('triggers addPlaylist function on submission', () => {
+
+})
+
+it ('triggers addPlaylist with the correct arguments on submission', () => {
+  
+})
