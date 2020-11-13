1fc0f150c6fc134917ae44f48c4811588a4f9172
charlie763 <cwisoff@gmail.com>
2020-11-13 09:34:19 -0700

    stub out tests to write for playlists

---
+++ src/components/playlists/playlists/__tests__/playlists.test.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+it ('renders without crashing', () => {
+
+})
+
+it ('matches the snapshot', () => {
+
+})
+
+it ('displays the usersname if the user is logged in', () => {
+
+})
+
+it ('routes to the new bookshelf route when clicking on add bookshelf', () => {
+
+})
+
+it ('displays the correct number of bookshelves', () => {
+
+})
+
+it ('routes to the bookshelf show route when user clicks on a bookshelf', () => {
+  
+})
+
